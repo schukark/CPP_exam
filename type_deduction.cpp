31a6e70cd1857ff03a3575194d36c1350e26d7f2
schukark <84895462+schukark@users.noreply.github.com>
2023-03-28 01:55:36 +0300

    Create type_deduction.cpp

---
+++ type_deduction.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include <vector>
+
+typename std::iterator<std::vector <int>> x = *itre
+
+auto x = obj -> приводит к типу автоматически
+
+const auto
+
+const auto&
+
+auto&&
+
+int&& a = 1;
+
+auto x = a; -> плохо
+
+decltype(a) b -> тип соответсвует изначальному
+
+decltype(a++) т.к. это функц. сущность
+
+decltype(auto) -> смотрит что возвращается и делать decltype()
+
